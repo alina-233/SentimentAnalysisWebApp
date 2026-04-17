cad250408bb6374c79b15415b5f506ef46bd0ecd
Anamta Mujeeb <anamtamujeeb17@gmail.com>
2026-04-17 11:29:24 +0530

    1 File changed

--- db.py
+++ db.py
@@ -1,15 +1,3 @@
-# import certifi
-# from pymongo import MongoClient
-
-# client = MongoClient(
-#     "mongodb+srv://anamtamujeeb17_db_user:AAn93eB0bwcqjAHX@cluster0.jvjfdv4.mongodb.net/?appName=Cluster0",
-#     tlsCAFile=certifi.where()
-# )
-# db = client["sentiment_app"]
-
-# users_collection = db["users"]
-# history_collection = db["history"]
-
 import certifi
 from pymongo import MongoClient
 import os
