2c28be8dad4d9be16c47e497dc63f89e6194c283
Anamta Mujeeb <anamtamujeeb17@gmail.com>
2026-04-17 11:28:32 +0530

    Final edits made

--- db.py
+++ db.py
@@ -1,10 +1,29 @@
+# import certifi
+# from pymongo import MongoClient
+
+# client = MongoClient(
+#     "mongodb+srv://anamtamujeeb17_db_user:AAn93eB0bwcqjAHX@cluster0.jvjfdv4.mongodb.net/?appName=Cluster0",
+#     tlsCAFile=certifi.where()
+# )
+# db = client["sentiment_app"]
+
+# users_collection = db["users"]
+# history_collection = db["history"]
+
 import certifi
 from pymongo import MongoClient
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+
+MONGO_URI = os.getenv("MONGO_URI")
 
 client = MongoClient(
-    "mongodb+srv://anamtamujeeb17_db_user:AAn93eB0bwcqjAHX@cluster0.jvjfdv4.mongodb.net/?appName=Cluster0",
+    MONGO_URI,
     tlsCAFile=certifi.where()
 )
+
 db = client["sentiment_app"]
 
 users_collection = db["users"]
